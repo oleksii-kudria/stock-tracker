a75e2297d1b7cf42b7c7312e06fb4baf37205143
Oleksii Kudria <38926096+oleksii-kudria@users.noreply.github.com>
2025-06-13 23:07:05 +0300

    Add configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+"""Global configuration variables for the stock tracker project."""
+
+# Перелік біржових тікерів, які необхідно моніторити
+STOCK_SYMBOLS = [
+    "AAPL",  # Apple Inc.
+    "GOOGL", # Alphabet Inc.
+    "AMZN",  # Amazon.com, Inc.
+    "MSFT",  # Microsoft Corporation
+    "TSLA"    # Tesla, Inc.
+]
